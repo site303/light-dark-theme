37f0d08d8d41c790cd15a72eafd0df4e097df47f
site303 <buranuranuragan@mail.ru>
2025-05-11 21:18:16 +0300

    add file form

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+import styles from '../styles/Theme.module.css';
+
+export default function Form() {
+    const { theme } = useContext(ThemeContext);
+    return(
+<div>
+    
+</div>
+    );
+};
